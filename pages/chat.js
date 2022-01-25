ae48d45cdd4c09c25f615edd1a8bbd4fbeaf8383
Luís Fernando Grange Ferreira <luisfernando237grange@outlook.com>
2022-01-25 14:01:37 -0300

    Initial commit

---
+++ pages/chat.js
@@ -0,0 +1,6 @@
+
+export default function PaginaDoChat(){
+    return(
+        <h1>Hello chat</h1>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,140 @@
+import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import appConfig from '../config.json';
+import React from 'react';
+import { useRouter } from 'next/router';
+
+
+
+function Title(props){
+    const Tag = props.tag || 'h1';
+    return(
+        <>
+            <Tag>{props.children}</Tag>
+            <style jsx>{`
+            ${Tag} {
+                color: ${appConfig.theme.colors.neutrals['000']};
+                font-size: 24px;
+                font-weight: 600;
+            }
+            `}</style>
+        </>
+    )
+}
+
+function PaginaInicial(){
+    //const username = 'luisgrange';
+    const [username, setUsername] = React.useState('');
+    const roteamento = useRouter();
+
+    return(
+        <>
+            <Box
+                styleSheet={{
+                    display: 'flex', alignItems: 'center', justifyContent: 'center',
+                    background: appConfig.theme.colors.primary['500'],
+                    backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/07/this-is-fine.jpeg)',
+                    backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'normal',
+                }}
+            >
+                <Box
+                    styleSheet={{
+                        display: 'flex', 
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                        flexDirection: {
+                            xs: 'column',
+                            sm: 'row',
+                        },
+                        width: '100%', maxWidth:'700px',
+                        borderRadius:'5px', padding:'32px', margin:'16px',
+                        boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                        backgroundColor: appConfig.theme.colors.neutrals[700],
+                    }}
+                >
+                    {/*FORMULÁRIO*/}
+                    <Box
+                        as="form"
+                        onSubmit={function(e){
+                            e.preventDefault();
+                            roteamento.push('/chat');
+                        }}
+                        styleSheet={{
+                            display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+                            width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+                        }}
+                    >
+                        <Title tag='h2'>Bem-vindos de volta!</Title>
+                        <Text variant='body3' styleSheet={{
+                            marginBottom: '32px', color: appConfig.theme.colors.neutrals[300]
+                        }}>
+                            {appConfig.name}
+                        </Text>
+
+                        <TextField
+                            fullWidth
+                            onChange={function(e){
+                                
+                                setUsername(e.target.value);
+                            }}
+                            textFieldColors={{
+                                neutral: {
+                                textColor: appConfig.theme.colors.neutrals[200],
+                                mainColor: appConfig.theme.colors.neutrals[900],
+                                mainColorHighlight: appConfig.theme.colors.primary[500],
+                                backgroundColor: appConfig.theme.colors.neutrals[800],
+                                },
+                            }}
+                            />
+                        <Button
+                            type='submit'
+                            label='Entrar'
+                            fullWidth
+                            buttonColors={{
+                                contrastColor: appConfig.theme.colors.neutrals["000"],
+                                mainColor: appConfig.theme.colors.primary[500],
+                                mainColorLight: appConfig.theme.colors.primary[400],
+                                mainColorStrong: appConfig.theme.colors.primary[600],
+                            }}
+                            />
+                    </Box>
+                    {/* Photo */}
+                    <Box
+                        styleSheet={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            maxWidth: '200px',
+                            padding: '16px',
+                            backgroundColor: appConfig.theme.colors.neutrals[800],
+                            border: '1px solid',
+                            borderColor: appConfig.theme.colors.neutrals[999],
+                            borderRadius: '10px',
+                            flex: 1,
+                            minHeight: '240px',
+                        }}
+                    >
+                        <Image 
+                            styleSheet={{ 
+                                borderRadius: '50%',
+                                marginBottom: '16px',
+                            }} src={`https://github.com/${username}.png`}/>
+
+                        <Text
+                            variant="body4"
+                            styleSheet={{
+                                color: appConfig.theme.colors.neutrals[200],
+                                backgroundColor: appConfig.theme.colors.neutrals[900],
+                                padding: '3px 10px',
+                                borderRadius: '1000px'
+                            }}
+                        >
+                            {username}
+                        </Text>
+                    </Box>
+                </Box>
+            </Box>
+        </>
+    );
+}
+
+export default PaginaInicial
